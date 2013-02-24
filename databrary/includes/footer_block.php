fab9b7a3de044000b126357decf7ef91f0f30ffc
Gladys Chan <gladys@databrary.org>
2013-02-24 15:53:43 -0500

    includes/footer_block.php

--- databrary/includes/footer_block.php
+++ databrary/includes/footer_block.php
@@ -18,7 +18,7 @@
     </ul>
 
     <p id="social_network">
-    Find us on <a href="http://www.facebook.com/pages/Databrary/185349568273416"><img id="facebook" src="<?php url_prefix(); ?>images/facebook.jpg" alt="Facebook"></a>, <a href="http://plus.google.com"><img id="google-plus" src="<?php url_prefix(); ?>images/gplus.png" alt="Google Plus"></a>, <a href="https://twitter.com/databrary"><img id="twitter" src="<?php url_prefix(); ?>images/twitter.png" alt="Twitter"></a>, and <a href="https://github.com/OpenSHAPA"><img id="github" src="<?php url_prefix(); ?>images/github.jpg" alt="GitHub"></a>.
+    Find us on <a href="http://www.facebook.com/pages/Databrary/185349568273416"><img id="facebook" src="<?php url_prefix(); ?>images/facebook.jpg" alt="Facebook"></a>, <a href="https://plus.google.com/u/1/115581407967674652034/posts"><img id="google-plus" src="<?php url_prefix(); ?>images/gplus.png" alt="Google Plus"></a>, <a href="https://twitter.com/databrary"><img id="twitter" src="<?php url_prefix(); ?>images/twitter.png" alt="Twitter"></a>, and <a href="https://github.com/databrary/"><img id="github" src="<?php url_prefix(); ?>images/github.jpg" alt="GitHub"></a>.
     </p>
 </footer>
 
