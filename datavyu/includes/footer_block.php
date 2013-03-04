ccd9de224aa32c8fa4e70ef70066621706a36f7f
rogilmore <thatrickgilmrore@gmail.com>
2013-03-04 15:33:43 -0500

    fixed Twitter link in footer to @datavyu

--- datavyu/includes/footer_block.php
+++ datavyu/includes/footer_block.php
@@ -18,7 +18,7 @@
     </ul>
 
     <p id="social_network">
-    Find us on <a href="http://www.facebook.com/pages/Databrary/185349568273416" target="_blank"><img id="facebook" src="/images/facebook.jpg" alt="Facebook"></a>, <a href="https://plus.google.com/u/1/115581407967674652034/posts" target="_blank"><img id="google-plus" src="/images/gplus.png" alt="Google Plus"></a>, <a href="https://twitter.com/databrary" target="_blank"><img id="twitter" src="/images/twitter.png" alt="Twitter"></a>, and <a href="https://github.com/databrary/datavyu" target="_blank"><img id="github" src="/images/github.jpg" alt="GitHub"></a>.
+    Find us on <a href="http://www.facebook.com/pages/Databrary/185349568273416" target="_blank"><img id="facebook" src="/images/facebook.jpg" alt="Facebook"></a>, <a href="https://plus.google.com/u/1/115581407967674652034/posts" target="_blank"><img id="google-plus" src="/images/gplus.png" alt="Google Plus"></a>, <a href="https://twitter.com/datavyu" target="_blank"><img id="twitter" src="/images/twitter.png" alt="Twitter"></a>, and <a href="https://github.com/databrary/datavyu" target="_blank"><img id="github" src="/images/github.jpg" alt="GitHub"></a>.
     </p>
 </footer>
 
