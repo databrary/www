42ad0e52c14539783608c3cf9658cb862f594b2d
Gladys Chan <gladys@databrary.org>
2013-03-06 15:05:54 -0500

    inserted fineprint

--- datavyu/includes/footer_block.php
+++ datavyu/includes/footer_block.php
@@ -20,5 +20,7 @@
     <p id="social_network">
     Find us on <a href="http://www.facebook.com/pages/Datavyu/393203144105508" target="_blank"><img id="facebook" src="/images/facebook.jpg" alt="Facebook"></a>, <a href="https://plus.google.com/u/1/115581407967674652034/posts" target="_blank"><img id="google-plus" src="/images/gplus.png" alt="Google Plus"></a>, <a href="https://twitter.com/databrary" target="_blank"><img id="twitter" src="/images/twitter.png" alt="Twitter"></a>, and <a href="https://github.com/databrary/datavyu" target="_blank"><img id="github" src="/images/github.jpg" alt="GitHub"></a>.
     </p>
+
+	<p id="fineprint" style="font-size:xx-small;color:#bbb;line-height:10px;padding-bottom:10px;text-align:left;">This material is based upon work supported by the National Science Foundation under Grant No. BCS-1238599. Any opinions, findings, and conclusions or recommendations expressed in this material are those of the author(s) and do not necessarily reflect the views of the National Science Foundation.</p>
 </footer>
 
