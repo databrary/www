c5ababd0abc1f1187df5d499b464f83b6b8d74de
Gladys Chan <gladys@databrary.org>
2013-03-18 10:56:00 -0400

    updated footer with sponsors

--- databrary/index.php
+++ databrary/index.php
@@ -9,11 +9,9 @@ $title = "Databrary";
 ?>
 <?php require "includes/header.php"; ?>
 
-<div id="splash"><img id="logo-splash" src="images/splash/databrary.png">
-<br><br>
-<h2>An open data library for developmental science. Share video, audio, and related metadata. <br>Discover more, faster.</h2>
+<div id="splash">
+<h2><span style="font-family:SofiaProMedium;">Data</span>brary is an <span style="color:#E01B6A;">open data library</span> for developmental science. Share video, audio, and related metadata. Discover more, faster.</h2>
 <br>
-<p>Funded by Grant No. BCS-1238599 from <img class="sponsors" src="images/nsf1.gif"></p>
 </div>
 
 <?php require "includes/footer.php"; ?>
