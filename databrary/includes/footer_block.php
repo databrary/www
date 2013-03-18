20f9e5638a972412029e1cd07568fb04eca0e265
Gladys Chan <gladys@databrary.org>
2013-03-18 14:46:36 -0400

    update mailchimp link

--- databrary/includes/footer_block.php
+++ databrary/includes/footer_block.php
@@ -19,7 +19,7 @@
 
     <p>
     <span id="sponsors">
-    <a href=""><img class="sponslogo" src="/images/nsf1.gif"></a> Grant No. BCS-1238599</span>
+    <a href="https://arc.uchicago.edu/reese/projects/databrary-open-video-based-data-sharing-system-developmental-science"><img class="sponslogo" src="/images/nsf1.gif"></a> Grant No. BCS-1238599</span>
     <span id="social_network">
     Find us on <a href="http://www.facebook.com/pages/Datavyu/393203144105508" target="_blank"><img id="facebook" src="/images/facebook.jpg" alt="Facebook"></a>, <a href="https://plus.google.com/u/1/115581407967674652034/posts" target="_blank"><img id="google-plus" src="/images/gplus.png" alt="Google Plus"></a>, <a href="https://twitter.com/databrary" target="_blank"><img id="twitter" src="/images/twitter.png" alt="Twitter"></a>, and <a href="https://github.com/databrary/datavyu" target="_blank"><img id="github" src="/images/github.jpg" alt="GitHub"></a>.
     </span></p>
