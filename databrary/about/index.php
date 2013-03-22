d511a9cfb3d6163fa7aa400cdf1a22ad3731911c
Amy C <amy.chang@nyu.edu>
2013-03-22 18:32:58 -0300

    Update index.php
    Page fixes, see change log

--- databrary/about/index.php
+++ databrary/about/index.php
@@ -13,12 +13,12 @@ $title = "About";
 	<h1 id="project">The Databrary Project</h1> 
 	<article>
 		<p>
-		Most developmental scientists rely on video recordings to capture the complexity and richness of behavior. Researchers rarely share video data, and this has impeded scientific progress. By creating the cyber-infrastructure and community to enable open video sharing, the Databrary project aims to facilitate deeper, richer, and broader understanding of behavior.
+		Most developmental scientists rely on video recordings to capture the complexity and richness of behavior. However, researchers rarely share video data, and this has impeded scientific progress. By creating the cyber-infrastructure and community to enable open video sharing, the Databrary project aims to facilitate deeper, richer, and broader understanding of behavior.
 		</p><p> 
 		The Databrary project includes:</p>
 		<ul>
 		<li>
-		<a href="/">Databrary</a>. A web-based repository for open sharing and preservation of video data and associated meta-data.
+		<a href="/">Databrary</a>. A web-based repository for open sharing and preservation of video data and associated metadata.
 		</li><li> 
 		<a href="http://datavyu.org">Datavyu</a>. A free, open source video-coding software, that enables coding, exploring, and analyzing video data.
 		</li><li> 
@@ -29,19 +29,19 @@ $title = "About";
 	</article>
 
 	<a name="policies">
-		<h1 id="policies">Policies</h1>
-		<article>
-			<h2>Values</h2>
+	<h1 id="policies">Policies</h1>
+	<article>
+		<h2>Values</h2>
 
-			<p>Databrary aims to increase openness in scientific research. We seek to enable researchers to share their findings with other scientists and the community at-large while protecting the privacy of individuals who participate in research. Participants and others depicted in recordings have the right to consent to having the recordings shared with Databrary. Their wishes must be respected.</p> 
+		<p>Databrary aims to increase openness in scientific research. We seek to enable researchers to share their findings with other scientists and the community at-large while protecting the privacy of individuals who participate in research. Participants and others depicted in recordings have the right to consent to having the recordings shared with Databrary. Their wishes must be respected.</p> 
 
-			<p>Achieving these goals requires that we create and maintain a community that embodies the highest ethical standards.</p>
+		<p>Achieving these goals requires that we create and maintain a community that embodies the highest ethical standards.</p>
 
-			<h2>Procedures</h2>
+		<h2>Procedures</h2>
 
-			<p>Databrary is governed by a set of standard operating procedures (SOPs). Individuals who wish to become users or contributors must apply for access. Only individuals who demonstrate knowledge of the principles of research ethics with human participants and who agree to the provisions of Databrary's user and/or contributor agreements will be granted access.</p> 
+		<p>Databrary is governed by a set of standard operating procedures (SOPs). Individuals who wish to become users must apply for access. Only individuals who demonstrate knowledge of the principles of research ethics with human participants and who agree to the provisions of Databrary's user and/or contributor agreements will be granted access.</p> 
 
-			<p>For more information about Databrary policies and procedures see the <a href="http://github.com/databrary/policies" target="_blank">github repository</a>.</p>
-		</article>
+		<p>For more information about Databrary policies and procedures see the <a href="http://github.com/databrary/policies" target="_blank">github repository</a>.</p>
+	</article>
 
 <?php require "../includes/footer.php"; ?>
