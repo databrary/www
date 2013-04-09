8c6b4fd35070eb862956505e515d0ac02b1afecc
Dylan Simon <dylan@dylex.net>
2013-04-09 08:52:40 -0400

    fix html

--- databrary/includes/footer_block.php
+++ databrary/includes/footer_block.php
@@ -18,8 +18,9 @@
     </ul>
     <p>
     <span id="sponsors">
-    <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1238599&HistoricalAwards=false"><img class="sponslogo" src="/images/nsf1.gif"></a> Grant No. BCS-1238599</span>
-    <a href="http://projectreporter.nih.gov/project_info_description.cfm?aid=8531595&icde=15908155&ddparam=&ddvalue=&ddsub=&cr=1&csb=default&cs=ASC"><img class="sponslogo" src="/images/nih-logo.jpg"></a> Cooperative Agreement No. 1-U01-HD-076595-01</span>
+    <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1238599&HistoricalAwards=false"><img class="sponslogo" src="/images/nsf1.gif"></a> Grant No. BCS-1238599
+    <a href="http://projectreporter.nih.gov/project_info_description.cfm?aid=8531595&icde=15908155&ddparam=&ddvalue=&ddsub=&cr=1&csb=default&cs=ASC"><img class="sponslogo" src="/images/nih-logo.jpg"></a> Cooperative Agreement No. 1-U01-HD-076595-01
+    </span>
     <span id="social_network">
     Find us on <a href="https://www.facebook.com/pages/Databrary/185349568273416" target="_blank"><img id="facebook" src="/images/facebook.jpg" alt="Facebook"></a>, <a href="https://plus.google.com/u/1/111083162045777800330/posts" target="_blank"><img id="google-plus" src="/images/gplus.png" alt="Google Plus"></a>, <a href="https://twitter.com/databrary" target="_blank"><img id="twitter" src="/images/twitter.png" alt="Twitter"></a>, and <a href="https://github.com/databrary/" target="_blank"><img id="github" src="/images/github.jpg" alt="GitHub"></a>.
     </span></p>
