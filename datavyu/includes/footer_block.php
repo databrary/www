339676e4268a7a95c5a7342356d3648d74922425
Gladys Chan <gladys@databrary.org>
2013-04-09 21:51:38 -0400

    footer sponsors change

--- datavyu/includes/footer_block.php
+++ datavyu/includes/footer_block.php
@@ -18,8 +18,8 @@
     </ul>
     <p>
     <span id="sponsors">
-    <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1238599&HistoricalAwards=false"><img class="sponslogo" src="/images/nsf1.gif"></a> Grant No. BCS-1238599
-    <a href="http://projectreporter.nih.gov/project_info_description.cfm?aid=8531595&icde=15908155&ddparam=&ddvalue=&ddsub=&cr=1&csb=default&cs=ASC"><img class="sponslogo" src="/images/nih-logo.jpg"></a> Cooperative Agreement No. 1-U01-HD-076595-01
+    <a href="http://www.nsf.gov/awardsearch/showAward?AWD_ID=1238599&HistoricalAwards=false"><img class="sponslogo" src="/images/nsf1.gif"></a> BCS-1238599
+    <a href="http://projectreporter.nih.gov/project_info_description.cfm?aid=8531595&icde=15908155&ddparam=&ddvalue=&ddsub=&cr=1&csb=default&cs=ASC"><img class="sponslogo" src="/images/nih-logo.jpg"></a> 1-U01-HD-076595
     </span>
     <span id="social_network">
     Find us on <a href="http://www.facebook.com/pages/Datavyu/393203144105508" target="_blank"><img id="facebook" src="/images/facebook.jpg" alt="Facebook"></a>, <a href="https://plus.google.com/u/1/115581407967674652034/posts" target="_blank"><img id="google-plus" src="/images/gplus.png" alt="Google Plus"></a>, <a href="https://twitter.com/databrary" target="_blank"><img id="twitter" src="/images/twitter.png" alt="Twitter"></a>, and <a href="https://github.com/databrary/datavyu" target="_blank"><img id="github" src="/images/github.jpg" alt="GitHub"></a>.
