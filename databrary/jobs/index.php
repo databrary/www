a3d229de96c337c1b723bf081c13d992f54b6f98
Dylan Simon <dylan@dylex.net>
2013-05-16 13:04:23 -0400

    add postdoc job

--- databrary/jobs/index.php
+++ databrary/jobs/index.php
@@ -59,6 +59,33 @@ $title = "Jobs";
 	</div>
 
 	<div class="form-container">
+	<h2>Postdoctoral Fellow</h2>
+	<p>
+		The Databrary project, in coordination with the Department of Psychology and Digital Library Services at NYU, seeks a cross-disciplinary postdoctoral fellow looking to expand their knowledge of research methods and technology practices in science and industry.
+		You will participate in design, development, and deployment of technology to support collaboration, exploration, and analysis of open research datasets.
+	</p>
+        <h3>Responsibilities</h3>
+        <ul>
+		<li>Design new approaches for structuring, presenting, and sharing research data.</li>
+		<li>Work with a small team of software developers to build web, mobile, and desktop software tools to manage, analyze, and visualize data.</li>
+		<li>Engage with researchers within and beyond the fields of developmental psychology, library science, and computer vision to collect, organize, and repurpose data.</li>
+        </ul>
+        <h3>Qualifications</h3>
+        <ul>
+		<li>Strong programming skills in at least two languages (such as Matlab, R, Python, Ruby, JavaScript)</li>
+		<li>Experience working with and analyzing videos, physiological data, imaging data, or other large datasets</li>
+		<li>Familiarity with standard development and collaboration tools such as git</li>
+		<li>Willingness to learn and work with the latest web-based technologies</li>
+        </ul>
+        <h3>Preferred</h3>
+        <ul>
+		<li>Understanding of web technologies including HTML, JavaScript, and MVC frameworks</li>
+		<li>Knowledge of SQL or other database technologies</li>
+		<li>History of collaboration and cooperation on previous projects</li>
+        </ul>
+	</div>
+
+	<div class="form-container">
         <h2>Web UI/UX Designer</h2>
         <p>
 		The Databrary project seeks a motivated and flexible UI/UX designer to help create a web-based data library.
