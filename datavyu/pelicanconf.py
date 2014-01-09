331fd7ab1821379f77572023c52c23a1e05299c3
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-01-09 17:50:54 -0500

    hack for sphinx non-html5; docs static content works; make html automatically compiles sphinx

--- datavyu/pelicanconf.py
+++ datavyu/pelicanconf.py
@@ -20,16 +20,26 @@ OUTPUT_RETENTION = ('maven2', 'releases', 'support')
 
 STATIC_PATHS.extend([
 	'releases',
-	'user-guide/.buildinfo',
-	'user-guide/objects.inv',
-	'user-guide/searchindex.js',
-	'user-guide/_downloads',
-	'user-guide/_images',
-	'user-guide/_sources',
-	'user-guide/_static',
+	'pages/user-guide/.buildinfo',
+	'pages/user-guide/objects.inv',
+	'pages/user-guide/searchindex.js',
+	'pages/user-guide/_downloads',
+	'pages/user-guide/_images',
+	'pages/user-guide/_sources',
+	'pages/user-guide/_static',
 ])
 
 TEMPLATE_PAGES = {
 	'../../../datavyu/version.txt': 'version.txt',
 	'../../../datavyu/pre_version.txt': 'pre_version.txt',
 }
+
+EXTRA_PATH_METADATA = {}
+
+for dirpath, dirnames, filenames in os.walk(os.curdir+'/datavyu/input/pages/user-guide'):
+    if '_sources' in dirpath:
+        continue
+
+    for filename in [filename for filename in filenames if '.html' not in filename]:
+        tmp = os.path.join(dirpath, filename)[2:].split('/')
+        EXTRA_PATH_METADATA['/'.join(tmp[2:])] = {'path': '/'.join(tmp[4:])}
