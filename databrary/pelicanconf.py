95797a412b15f4ffc4f8d3c0e75ddef148c1c234
Dylan Simon <dylan@dylex.net>
2015-04-23 14:48:15 -0400

    Fix some links

--- databrary/pelicanconf.py
+++ databrary/pelicanconf.py
@@ -13,9 +13,9 @@ from common.pelicanconf import *
 # SITE
 SITENAME = 'Databrary: An Open Data Library for Developmental Science'
 SITESLUG = 'databrary'
-TOPRIGHT_TARGET1 = '//nyu.databrary.org/register'
+TOPRIGHT_TARGET1 = 'https://nyu.databrary.org/register'
 TOPRIGHT_TEXT1   = 'register'
-TOPRIGHT_TARGET2 = '//nyu.databrary.org/login'
+TOPRIGHT_TARGET2 = 'https://nyu.databrary.org/login'
 TOPRIGHT_TEXT2 = 'login'
 
 # COMPILE
