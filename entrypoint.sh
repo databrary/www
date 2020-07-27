d2ff0d64665c68068fbbc70476b8f9eb81f9a575
Jeffrey Spies <code@jeffspies.com>
2020-07-27 15:59:40 -0400

    Incrementally testing.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 cd /build/www
 make update-repos
 echo $INPUT_GITHUB_TOKEN $GITHUB_SHA
-INPUT_GITHUB_TOKEN=$INPUT_GITHUB_TOKEN GITHUB_SHA=$GITHUB_SHA make $1
+make $1
