b73e308c33274274e726bec557c7ab9e8a7f3c7a
Jeffrey Spies <code@jeffspies.com>
2020-07-27 15:09:23 -0400

    Fixing github inputs?

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 git config --global user.name "$GITHUB_ACTOR"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 cd /build/www
-make $1
+GITHUB_SHA=$GITHUB_SHA INPUT_GITHUB_TOKEN=$INPUT_GITHUB_TOKEN make $1
